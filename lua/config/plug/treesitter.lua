b952014f12654eb69a1d49db80bfaae2dae352cd
Apeiros-46B <Apeiros-46B@users.noreply.github.com>
2022-09-14 20:33:36 -0700

    ensure python TS installed

--- lua/config/plug/treesitter.lua
+++ lua/config/plug/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup({
 	-- One of "all", "maintained" (parsers with maintainers), or a list of languages
-	ensure_installed = { 'lua', 'java', 'go', 'rust', 'c', 'cpp', 'julia', 'haskell', 'norg' },
+	ensure_installed = { 'lua', 'java', 'go', 'rust', 'c', 'cpp', 'julia', 'haskell', 'python', 'norg' },
 
 	-- Install languages synchronously (only applied to `ensure_installed`)
 	sync_install = false,
