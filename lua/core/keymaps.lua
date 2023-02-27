53b65885651b58c5d41133fb579201616b899f37
Apeiros-46B <Apeiros-46B@users.noreply.github.com>
2023-02-26 22:01:48 -0800

    fix keymap options

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,14 +7,9 @@ local function map(mode, bind, exec, opts)
         options = vim.tbl_extend('force', options, opts)
     end
 
-    vim.api.nvim_set_keymap(mode, bind, exec, opts)
+    vim.api.nvim_set_keymap(mode, bind, exec, options)
 end
 
--- local function unmap(mode, bind)
---     vim.api.nvim_del_keymap(mode, bind)
--- end
-
-local opt = { silent = true } -- default opts
 local M = {}
 
 vim.g.mapleader = ' ' -- map leader key to space
@@ -26,40 +21,37 @@ vim.g.maplocalleader = ','
 -- }}}
 
 -- {{{ mapping
--- note that these are only vanilla vim keymaps, all plugin keymaps are handled in `plugins.lazy`
--- {{{ misc
-map('n', 'WW',    '<Cmd>w<CR>',    opt) -- write shortcut
-map('n', '<C-j>', '<Cmd>join<CR>', opt)
--- }}}
-
+-- note that these are only vanilla nvim keymaps, all plugin keymaps are handled in `plugins.lazy`
 -- {{{ [b] buffer
-map('n', 'J', '<Cmd>bp<CR>', opt)
-map('n', 'K', '<Cmd>bn<CR>', opt)
+map('n', '<M-x>', '<Cmd>bd!<CR>')
+map('n', 'X',     '<Cmd>bd<CR>')
+map('n', 'J',     '<Cmd>bp<CR>')
+map('n', 'K',     '<Cmd>bn<CR>')
 
-map('n', '<leader>bd', '<Cmd>bd<CR>', opt)
-map('n', '<leader>bh', '<Cmd>bp<CR>', opt)
-map('n', '<leader>bj', '<Cmd>bf<CR>', opt)
-map('n', '<leader>bk', '<Cmd>bl<CR>', opt)
-map('n', '<leader>bl', '<Cmd>bn<CR>', opt)
+map('n', '<leader>bd', '<Cmd>bd<CR>')
+map('n', '<leader>bh', '<Cmd>bp<CR>')
+map('n', '<leader>bj', '<Cmd>bf<CR>')
+map('n', '<leader>bk', '<Cmd>bl<CR>')
+map('n', '<leader>bl', '<Cmd>bn<CR>')
 -- }}}
 
 -- {{{ [F] format
 -- get rid of annoying curly quotation marks and apostrophes
-map('n', '<leader>Fc', [[<Cmd>%s/[“”‟]/"/g | %s/[‘’‛]/'/g<CR>]], opt)
+map('n', '<leader>Fc', [[<Cmd>%s/[“”‟]/"/g | %s/[‘’‛]/'/g<CR>]])
 -- }}}
 
 -- {{{ [l] lsp
 -- show floating diagnostic
-map('n', '<leader>d', '<Cmd>lua vim.diagnostic.open_float()<CR>', opt)
+map('n', '<leader>d', '<Cmd>lua vim.diagnostic.open_float()<CR>')
 -- }}}
 
 -- {{{ [t] tab
-map('n', '<leader>td', '<Cmd>tabclose<CR>', opt)
-map('n', '<leader>th', '<Cmd>tabp<CR>',     opt)
-map('n', '<leader>tj', '<Cmd>tabfirst<CR>', opt)
-map('n', '<leader>tk', '<Cmd>tablast<CR>',  opt)
-map('n', '<leader>tl', '<Cmd>tabn<CR>',     opt)
-map('n', '<leader>tn', '<Cmd>tabnew<CR>',   opt)
+map('n', '<leader>td', '<Cmd>tabclose<CR>')
+map('n', '<leader>th', '<Cmd>tabp<CR>'    )
+map('n', '<leader>tj', '<Cmd>tabfirst<CR>')
+map('n', '<leader>tk', '<Cmd>tablast<CR>' )
+map('n', '<leader>tl', '<Cmd>tabn<CR>'    )
+map('n', '<leader>tn', '<Cmd>tabnew<CR>'  )
 -- }}}
 
 -- {{{ [T] toggle
@@ -68,15 +60,23 @@ map('n', '<leader>TW', '<Cmd>set wrap!<CR>', {}) -- word wrap
 -- }}}
 
 -- {{{ [y] yank
-map('n', '<leader>ya', '<Cmd>%y+<CR>', opt) -- yank entire buffer to system clipboard
-map('',  '<leader>ys',  '"+y',         opt) -- yank selection (or motion) into system clipboard
-map('n', '<leader>yy', '"+yy',         opt) -- yank current line into system clipboard
+map('n', '<leader>ya', '<Cmd>%y+<CR>') -- yank entire buffer to system clipboard
+map('',  '<leader>ys',  '"+y'        ) -- yank selection (or motion) into system clipboard
+map('n', '<leader>yy', '"+yy'        ) -- yank current line into system clipboard
 -- }}}
 
 -- {{{ [<CR>] terminal
-map('n', '<leader><CR><CR>', '<Cmd>terminal<CR>i', opt)
-map('n', '<leader><CR>v',    '<Cmd>vs | terminal<CR>i', opt)
-map('n', '<leader><CR>h',    '<Cmd>sp | terminal<CR>i', opt)
+map('n', '<leader><CR><CR>', '<Cmd>terminal<CR>i')
+map('n', '<leader><CR>v',    '<Cmd>vs | terminal<CR>i')
+map('n', '<leader><CR>h',    '<Cmd>sp | terminal<CR>i')
+-- }}}
+
+-- {{{ misc
+-- write shortcuts
+map('n', 'WW',    '<Cmd>w<CR>')
+map('n', '<M-w>', '<Cmd>w<CR>')
+
+map('', '<C-j>', 'J')
 -- }}}
 -- }}}
 
