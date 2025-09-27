6bd906baa4855cebb765af968bba6af3d3b813f2
Apeiros-46B <Apeiros-46B@users.noreply.github.com>
2025-09-27 13:31:16 -0700

    add venn.nvim

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -195,4 +195,34 @@ return {
 	-- 		}
 	-- 	end
 	-- },
+	{
+		'jbyuki/venn.nvim',
+		cmd = { 'VBox' },
+		keys = {
+			{
+				'<leader>v',
+				function()
+					local venn_enabled = vim.inspect(vim.b.venn_enabled)
+					if venn_enabled == "nil" then
+						vim.b.venn_enabled = true
+						vim.cmd 'setlocal ve=all'
+						vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", { noremap = true })
+						vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", { noremap = true })
+						vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", { noremap = true })
+						vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", { noremap = true })
+						vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>",       { noremap = true })
+					else
+						vim.b.venn_enabled = nil
+						vim.cmd 'setlocal ve='
+						vim.api.nvim_buf_del_keymap(0, "n", "H")
+						vim.api.nvim_buf_del_keymap(0, "n", "J")
+						vim.api.nvim_buf_del_keymap(0, "n", "K")
+						vim.api.nvim_buf_del_keymap(0, "n", "L")
+						vim.api.nvim_buf_del_keymap(0, "v", "f")
+					end
+				end,
+				mode = 'n'
+			}
+		},
+	},
 }
